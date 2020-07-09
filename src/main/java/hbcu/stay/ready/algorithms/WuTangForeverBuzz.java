c97b81e500345b437bd926a9ef9888e566327b53
stampedsteve <67400828+stampedsteve@users.noreply.github.com>
2020-07-09 16:12:42 -0400

    Finished algo 2

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -1,8 +1,22 @@
 package hbcu.stay.ready.algorithms;
 
 public class WuTangForeverBuzz {
-
+    String result = "";
     public String wuTangClan(){
-        return null;
+        for(int i=1; i<16;i++){
+            if(i % 3 == 0 && i % 5 ==0){
+                result += "WuTang Forever";
+            }
+            else if(i % 3 == 0){
+                result += "Wu\n";
+            }
+            else if(i % 5 == 0){
+                result += "Tang\n";
+            }
+            else{
+                result += (i + "\n"); 
+            }
+        }
+        return result;
     }
 }
